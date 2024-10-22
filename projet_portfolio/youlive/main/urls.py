c7f59c25dfce267705b3b0ca3ff68b517dbce884
Ivy1610 <8682@holbertonschool.com>
2024-10-22 14:25:21 +0200

    Refactor views and URLs for new pages

--- projet_portfolio/youlive/main/urls.py
+++ projet_portfolio/youlive/main/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='Contact - YOULIVE'),
     path('gallery/', views.gallery, name='Galerie - YOULIVE'),
+    path('events/', views.events, name='Événements'),
+    path('eventPage/', views.eventPage, name='Détails de l\'Événement - YOULIVE'),
+    path('authEvent/', views.authEvent, name='Authentificationde l\'événement'),
+    path('services/', views.services, name='Prestations - YOULIVE'),
 ]
--- projet_portfolio/youlive/youlive/views.py
+++ projet_portfolio/youlive/youlive/views.py
@@ -11,3 +11,14 @@ def contact(request):
 def gallery(request):
     return render(request, 'main/gallery.html')
 
+def events(request):
+    return render(request, 'main/events.html')
+
+def eventPage(request):
+    return render(request, 'main/eventPage.html')
+
+def authEvent(request):
+    return render(request, 'main/authEvent.html')
+
+def services(request):
+    return render(request, 'main/services.html')
