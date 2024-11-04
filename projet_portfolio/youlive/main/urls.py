ea42745ef88d0a9280eb4ad04b17956b4505b056
Ivy1610 <8682@holbertonschool.com>
2024-11-04 10:54:01 +0100

    Add social media links and privacy policy/terms of service pages; enhance footer styles

--- projet_portfolio/youlive/main/urls.py
+++ projet_portfolio/youlive/main/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('gallery/', views.gallery, name='Galerie - YOULIVE'),
     path('events/', views.events, name='Événements'),
     path('events/eventPage.html/', views.event_page, name='events - YOULIVE'),
-    path('eventPage/authEvent.html/', views.auth_event, name='Authentificationde l\'événement - YOULIVE'),
+    path('eventPage/authEvent.html/', views.auth_event, name='Authentification de l\'événement - YOULIVE'),
     path('services/', views.services, name='Prestations - YOULIVE'),
+    path('private_Policy/', views.private_policy, name='Politique de confidentialité - YOULIVE'),
+    path('terms_of_service/', views.terms_of_service, name='Terms of Service - YOULIVE'),
 ]
