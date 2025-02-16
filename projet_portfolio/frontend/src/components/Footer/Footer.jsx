53b16fb9c7238a4a1280d6700ffbfa151e45b33a
Ivy1610 <8682@holbertonschool.com>
2025-02-16 03:19:52 +0100

    Refactor carousel component: reintroduce Carousel.jsx with updated styles and functionality, and enhance Footer and Header components with improved structure and styling

--- projet_portfolio/frontend/src/components/Footer/Footer.jsx
+++ projet_portfolio/frontend/src/components/Footer/Footer.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import './footer.css';
 
 const Footer = () => {
   return (
-    <footer>
-      <p>© 2024 YOULIVE. Tous droits réservés.</p>
-      <div>
-        <a href="#">Facebook</a>
-        <a href="#">Instagram</a>
-        <a href="#">Twitter</a>
-        <a href="#">LinkedIn</a>
+    <footer className="footer">
+      <p>&copy; 2024 YOULIVE. Tous droits réservés.</p>
+      <div className="social-media">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-instagram"></i>
+        </a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-twitter"></i>
+        </a>
+        <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-linkedIn-in"></i>
+        </a>
+      </div>
+      <div className="footer-links">
+        <a href="/private-policy">Politique de confidentialité</a>
+        <a href="/terms-of-service">Conditions d'utilisation</a>
       </div>
     </footer>
   );
--- projet_portfolio/frontend/src/components/Header/Header.jsx
+++ projet_portfolio/frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import './Header.css';
+import './header.css';
 
 const Header = () => {
   const location = useLocation();
